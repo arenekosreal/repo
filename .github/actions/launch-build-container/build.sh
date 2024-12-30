8a0d2b4136f455b0024b6f319fe9bf73bc98739f
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-12-30 18:23:55 +0800

    Update launch-build-container

--- .github/actions/launch-build-container/build.sh
+++ .github/actions/launch-build-container/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-if ! grep -Fq "[$INPUT_CUSTOM_REPO]"
-then 
-    echo -e "[$INPUT_CUSTOM_REPO]\nServer = file:///var/lib/repo\nSigLevel = Optional TrustAll" >> /etc/pacman.conf
+if [[ ! -f /startdir/PKGBUILD ]]
+then
+    echo "PKGBUILD is not found"
+    ls -l /startdir
 fi
-pacman -Syu --noconfirm
+
+cd /startdir
 
 sudo -u builder -E BUILDDIR=/build -E PKGDEST=/pkgdest -E SRCDEST=/srcdest \
     makepkg --syncdeps --noconfirm --nosign
