816324d2533df07cb66571e1262c5587e49c8ea5
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-12-31 18:34:02 +0800

    Add message

--- .github/actions/update-pacman-repo/update.sh
+++ .github/actions/update-pacman-repo/update.sh
@@ -2,7 +2,9 @@
 
 if [[ -z "$INPUT_PACKAGES" ]]
 then
+  echo "Creating new repository at $INPUT_PATH_TO_REPO"
   /usr/bin/repo-add "$INPUT_PATH_TO_REPO"
 else
+  echo "Adding packages to existing repo"
   /usr/bin/repo-add "$INPUT_PATH_TO_REPO" $INPUT_PACKAGES
 fi
