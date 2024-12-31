5f2907bd65b6881ec979034d5540e72e0a8f7b49
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-12-31 19:12:14 +0800

    Fix build

--- .github/actions/scan-gpg-keys/scan.sh
+++ .github/actions/scan-gpg-keys/scan.sh
@@ -8,8 +8,8 @@ then
   exit 1
 fi
 
-cp -r "/github/workspace/$INPUT_DIRECTORY" /tmp/pkgbuild
-cd /tmp/pkgbuild
+cp -r "/github/workspace/$INPUT_DIRECTORY/." /pkgbuild
+cd /pkgbuild
 
 echo -n "gpg-key="
 sudo -u builder makepkg --printsrcinfo | grep validpgpkeys | cut -d = -f 2 | xargs >> "$GITHUB_OUTPUT"
