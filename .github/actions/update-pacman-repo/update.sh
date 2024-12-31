23b6ede1c327714f941ede0943dde763b7546267
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-12-31 18:39:29 +0800

    Fix build

--- .github/actions/update-pacman-repo/update.sh
+++ .github/actions/update-pacman-repo/update.sh
@@ -2,9 +2,9 @@
 
 if [[ -z "$INPUT_PACKAGES" ]]
 then
-  echo "Creating new repository at $INPUT_PATH_TO_REPO"
-  /usr/bin/repo-add "$INPUT_PATH_TO_REPO"
+  echo "Creating new repository at $INPUT_REPO"
+  /usr/bin/repo-add "$INPUT_PATH_REPO"
 else
   echo "Adding packages to existing repo"
-  /usr/bin/repo-add "$INPUT_PATH_TO_REPO" $INPUT_PACKAGES
+  /usr/bin/repo-add "$INPUT_PATH_REPO" $INPUT_PACKAGES
 fi
