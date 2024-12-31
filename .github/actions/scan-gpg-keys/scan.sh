4dfaf514407df2462af3b474ea656eb68017b9ab
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-12-31 18:17:06 +0800

    Scan gpg key in PKGBUILD

---
+++ .github/actions/scan-gpg-keys/scan.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+set -e
+
+if [[ ! -f "$INPUT_PATH_TO_DIRECTORY/PKGBUILD" ]]
+then
+  echo "No PKGBUILD is found."
+  exit 1
+fi
+
+cd "$INPUT_PATH_TO_DIRECTORY"
+
+echo -n ""
+makepkg --printsrcinfo | grep validpgpkeys | cut -d = -f 2 | xargs >> "$GITHUB_OUTPUT"
