0a0f9619161215f369b23a6c0e0adee7960d700f
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2025-01-04 18:54:31 +0800

    Fix find

--- .github/actions/update-pacman-repo/update.sh
+++ .github/actions/update-pacman-repo/update.sh
@@ -5,6 +5,6 @@ cd /github/workspace
 /usr/bin/repo-add "$INPUT_REPO"
 
 find "$(dirname "INPUT_REPO")" \
-  -maxdepth 0 -mindepth 0 -type f \
+  -maxdepth 1 -mindepth 1 -type f \
   -name '*.pkg.tar.*' \
   -exec /usr/bin/repo-add "$INPUT_REPO" {} +
