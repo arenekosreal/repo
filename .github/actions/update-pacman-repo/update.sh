fa1d608e79fc27ca0104ab7a5a1a19968e230f13
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2025-01-04 19:13:55 +0800

    Display filesystem status

--- .github/actions/update-pacman-repo/update.sh
+++ .github/actions/update-pacman-repo/update.sh
@@ -4,7 +4,10 @@ cd /github/workspace
 
 /usr/bin/repo-add "$INPUT_REPO"
 
-find "$(dirname "INPUT_REPO")" \
+find "$(dirname "$INPUT_REPO")" \
   -maxdepth 1 -mindepth 1 -type f \
   -name '*.pkg.tar.*' \
   -exec /usr/bin/repo-add "$INPUT_REPO" {} +
+find "$(dirname "$INPUT_REPO")" \
+  -maxdepth 1 -mindepth 1 -type f \
+  -name '*.pkg.tar.*' 
